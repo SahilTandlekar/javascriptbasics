66e9a7565e2767ea71f4711dcf6790e961b51b93
SAHIL <sahiltandlekar@gmail.com>
2026-07-14 08:46:55 +0530

    Identifiers

---
+++ Identifier.js
@@ -0,0 +1,9 @@
+var a = 10;
+console.log(a);
+
+var name = "Sahil";
+
+var firstName = "Sahillll";
+var This_is_a_very_long_name_variable = "PraSahilllllllllllll";
+var lastName = "Tandlekar";
+console.log(name + " " + lastName);
