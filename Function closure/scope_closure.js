9fa2f32d1f31675edb8231a2937ce302ededa855
SAHIL <sahiltandlekar@gmail.com>
2026-08-08 15:41:50 +0530

    sorted

--- Function closure/scope_closure.js
+++ Function closure/scope_closure.js
@@ -4,6 +4,6 @@ function setup(){
     console.log(env);
     console.log(timeout);
 }
-// setup();
-// console.log(env);
-// console.log(timeout);
+setup();
+console.log(env);
+console.log(timeout); //reference error since timeout is not defined outside
